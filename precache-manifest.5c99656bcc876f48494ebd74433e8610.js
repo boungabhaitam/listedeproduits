8684c88b25f1c943fefe9d1a862ad6c27566ad86
pouxulis <poxulis.haxis96@gmail.com>
2021-01-22 01:00:27 +0000

    Updates

---
+++ precache-manifest.5c99656bcc876f48494ebd74433e8610.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7c9c08d556aaf9f6728f5e3dc2df3d",
+    "url": "/ProductsList/index.html"
+  },
+  {
+    "revision": "8ba3283b5e9b188311d4",
+    "url": "/ProductsList/static/css/2.b0e270da.chunk.css"
+  },
+  {
+    "revision": "6994faed919ae62bc8b7",
+    "url": "/ProductsList/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8ba3283b5e9b188311d4",
+    "url": "/ProductsList/static/js/2.b443a846.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ProductsList/static/js/2.b443a846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6994faed919ae62bc8b7",
+    "url": "/ProductsList/static/js/main.6ee97e9d.chunk.js"
+  },
+  {
+    "revision": "ad1f9f28880fcd38d3f6",
+    "url": "/ProductsList/static/js/runtime-main.deab9ed5.js"
+  }
+]);
